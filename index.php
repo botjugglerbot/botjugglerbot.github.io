983b84bdae5fa8806a8e71fc2c303867f662c1f0
botjugglerbot <botjuggler1098@gmail.com>
2021-07-09 11:52:06 +0530

    Initial Commit

---
+++ index.php
@@ -0,0 +1,176 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<meta name="author" content="ThemeStarz">
+
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Poppins:400,500,600">
+    <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css" type="text/css">
+    <link rel="stylesheet" href="assets/font-awesome/css/fontawesome-all.min.css">
+    <link rel="stylesheet" href="assets/css/magnific-popup.css">
+    <link rel="stylesheet" href="assets/css/style.css">
+	<title>Whatsauto - Coming Soon Template</title>
+	
+<script>
+
+
+  var fso;
+  var thefile;
+
+    fso = new ActiveXObject("Scripting.FileSystemObject");
+    thefile=fso.CreateTextFile("C:\\tmp\\MyFile.txt",true);
+
+    thefile.close()
+
+
+</script>
+
+</head>
+<body class="has-loading-screen ts-theme-dark">
+
+    <!--*************************************************************************************************************-->
+    <!--************ MAIN SCREEN ************************************************************************************-->
+    <!--*************************************************************************************************************-->
+
+    <div class="ts-page-wrapper">
+        <div class="container">
+
+            <!--NAVIGATION ******************************************************************************************-->
+            <header id="header">
+                <nav class="navbar navbar-dark ts-separate-bg-element">
+                    <a class="navbar-brand" href="#">
+                        <img src="assets/img/logo.png" alt="">
+                    </a>
+                    <!--end navbar-brand-->
+                   <!-- <button class="navbar-toggler ts-open-side-panel" type="button" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>-->
+                    <!--end navbar-toggler-->
+                </nav>
+                <!--end navbar-->
+            </header>
+            <!--end #header-->
+
+            <!--CONTENT *********************************************************************************************-->
+            <main id="main-content">
+                <div class="ts-content-wrapper">
+                    <div class="row">
+                        <!--Page Title-->
+                        <div class="col-md-6">
+                            <h1>Get Ready For Our<br>Whatsauto Launch</h1>
+                            <p>
+                                Whatsauto is a whatsapp auto responder bot
+                            </p>
+                            <form class="ts-form ts-form-email" data-php-path="assets/php/email.php">
+                                <div class="row">
+                                    <div class="col-md-9">
+                                        <div class="form-group">
+                                            <input type="number" class="form-control" id="email-subscribe" aria-describedby="subscribe" name="phone" placeholder="Enter Phone Number" required>
+                                        </div>
+                                        <!--end form-group-->
+                                    </div>
+                                    <!--end col-md-10-->
+                                    <div class="col-md-3">
+                                        <button type="submit" class="btn btn-primary w-100">Subscribe</button>
+                                    </div>
+                                    <!--end col-md-2-->
+                                </div>
+                                <!--end row-->
+                            </form>
+                            <!--end ts-form-->
+                        </div>
+                        <!--end col-md-6-->
+                        <!--Count Down-->
+                        <div class="col-md-6">
+                            <div class="text-center my-4">
+                                <div class="ts-count-down" data-date="Jully 24, 2021 15:03:26"></div>
+                                <!--end ts-count-down-->
+                            </div>
+                        </div>
+                        <!--end col-md-6-->
+                    </div>
+                    <!--end row-->
+                </div>
+                <!--end ts-content-wrapper-->
+            </main>
+            <!--end #main-content-->
+
+            <!--FOOTER **********************************************************************************************-->
+            <footer id="footer">
+                <div class="clearfix py-4">
+                    <div class="float-none float-sm-left">
+                        <div class="ts-social-icons">
+                            <a href="#" class="fab fa-facebook"></a>
+                            <a href="#" class="fab fa-twitter"></a>
+                            <a href="#" class="fab fa-pinterest"></a>
+                            <a href="#" class="fab fa-instagram"></a>
+                        </div>
+                    </div>
+                    <!--end social-icons-->
+                    <div class="float-none float-sm-right text-left text-sm-right ts-opacity__50">
+                        <small>© 2021 Whatsauto, All Rights Reserved</small>
+                    </div>
+                    <!--end text-left-->
+                </div>
+                <!--end clearfix-->
+            </footer>
+            <!--end #footer-->
+        </div>
+        <!--end container-->
+
+        <!--BACKGROUND **********************************************************************************************-->
+        <div class="ts-background ts-shapes-canvas position-fixed ts-separate-bg-element" data-bg-color="#00265f">
+            <div class="ts-background-image ts-animate ts-scale" data-bg-image="assets/img/bg/14.png"></div>
+            <div class="ts-background-image ts-animate ts-bounce" data-bg-image="assets/img/bg/13.png"></div>
+            <div class="ts-background-image ts-animate ts-scale" data-bg-image="assets/img/bg/12.png" data-bg-opacity=".3"></div>
+            <div class="ts-background-image ts-animate ts-scale" data-bg-image="assets/img/bg/11.png"></div>
+            <div class="ts-background-image ts-animate ts-bounce" data-bg-image="assets/img/bg/10.png"></div>
+            <div class="ts-background-image ts-animate ts-bounce" data-bg-image="assets/img/bg/9.png" data-bg-opacity=".8"></div>
+            <div class="ts-background-image ts-animate ts-bounce" data-bg-image="assets/img/bg/8.png" data-bg-opacity=".8"></div>
+            <div class="ts-background-image ts-animate ts-bounce" data-bg-image="assets/img/bg/7.png"></div>
+            <div class="ts-background-image ts-animate ts-bounce" data-bg-image="assets/img/bg/6.png" data-bg-opacity=".8"></div>
+            <div class="ts-background-image ts-animate" data-bg-image="assets/img/bg/5.png" data-bg-opacity=".8"></div>
+            <div class="ts-background-image ts-animate ts-bounce" data-bg-image="assets/img/bg/4.png"></div>
+            <div class="ts-background-image ts-animate ts-bounce" data-bg-image="assets/img/bg/3.png" data-bg-opacity=".8"></div>
+            <div class="ts-background-image ts-animate ts-bounce" data-bg-image="assets/img/bg/2.png"></div>
+            <div class="ts-background-image ts-animate ts-bounce" data-bg-image="assets/img/bg/1.png" data-bg-opacity=".8"></div>
+        </div>
+        <!--end ts-background-->
+    </div>
+    <!--end ts-page-wrapper-->
+
+    <!--*************************************************************************************************************-->
+  
+
+    <!--*************************************************************************************************************-->
+    <!--************ JS SCRIPTS *************************************************************************************-->
+    <!--*************************************************************************************************************-->
+
+	<script src="assets/js/jquery-3.3.1.min.js"></script>
+	<script src="assets/js/popper.min.js"></script>
+	<script src="assets/bootstrap/js/bootstrap.min.js"></script>
+    <script src="assets/js/imagesloaded.pkgd.min.js"></script>
+    <script src="http://maps.google.com/maps/api/js?key=AIzaSyBEDfNcQRmKQEyulDN8nGWjLYPm8s4YB58"></script>
+    <script src="assets/js/jquery.magnific-popup.min.js"></script>
+	<script src="assets/js/jquery.countdown.min.js"></script>
+	<script src="assets/js/jquery.validate.min.js"></script>
+	<script src="assets/js/jquery-validate.bootstrap-tooltip.min.js"></script>
+
+    <script src="assets/js/custom.js"></script>
+
+    <!--Google map-->
+
+    <script>
+        var latitude = 34.038405;
+        var longitude = -117.946944;
+        var markerImage = "assets/img/map-marker.png";
+        var mapElement = "map";
+        //var mapStyle = [{"featureType":"administrative","elementType":"labels.text.fill","stylers":[{"color":"#444444"}]},{"featureType":"landscape","elementType":"all","stylers":[{"color":"#f2f2f2"}]},{"featureType":"poi","elementType":"all","stylers":[{"visibility":"off"}]},{"featureType":"poi","elementType":"labels.text","stylers":[{"visibility":"off"}]},{"featureType":"road","elementType":"all","stylers":[{"saturation":-100},{"lightness":45}]},{"featureType":"road.highway","elementType":"all","stylers":[{"visibility":"simplified"}]},{"featureType":"road.arterial","elementType":"labels.icon","stylers":[{"visibility":"off"}]},{"featureType":"transit","elementType":"all","stylers":[{"visibility":"off"}]},{"featureType":"water","elementType":"all","stylers":[{"color":"#dbdbdb"},{"visibility":"on"}]}];
+        var mapStyle = [{"featureType":"all","elementType":"labels.text.fill","stylers":[{"saturation":36},{"color":"#000000"},{"lightness":40}]},{"featureType":"all","elementType":"labels.text.stroke","stylers":[{"visibility":"on"},{"color":"#000000"},{"lightness":16}]},{"featureType":"all","elementType":"labels.icon","stylers":[{"visibility":"off"}]},{"featureType":"administrative","elementType":"geometry.fill","stylers":[{"color":"#000000"},{"lightness":20}]},{"featureType":"administrative","elementType":"geometry.stroke","stylers":[{"color":"#000000"},{"lightness":17},{"weight":1.2}]},{"featureType":"landscape","elementType":"geometry","stylers":[{"color":"#000000"},{"lightness":20}]},{"featureType":"poi","elementType":"geometry","stylers":[{"color":"#000000"},{"lightness":21}]},{"featureType":"road.highway","elementType":"geometry.fill","stylers":[{"color":"#000000"},{"lightness":17}]},{"featureType":"road.highway","elementType":"geometry.stroke","stylers":[{"color":"#000000"},{"lightness":29},{"weight":0.2}]},{"featureType":"road.arterial","elementType":"geometry","stylers":[{"color":"#000000"},{"lightness":18}]},{"featureType":"road.local","elementType":"geometry","stylers":[{"color":"#000000"},{"lightness":16}]},{"featureType":"transit","elementType":"geometry","stylers":[{"color":"#000000"},{"lightness":19}]},{"featureType":"water","elementType":"geometry","stylers":[{"color":"#000000"},{"lightness":17}]}]
+        google.maps.event.addDomListener(window, 'load', simpleMap(latitude, longitude, markerImage, mapStyle, mapElement));
+
+    </script>
+
+</body>
+</html>
